68dd10bca247dca189d5fd7b41eef58825b72776
Roy L Doyal <rldoyal@hotmail.com>
2018-09-15 09:24:50 -0700

    Change BookStore to Int32 from String

--- P2_598_Doyal_Aletto/OrderObject.cs
+++ P2_598_Doyal_Aletto/OrderObject.cs
@@ -8,7 +8,7 @@ namespace P2_598_Doyal_Aletto
 {
     public class OrderObject
     {
-        private string bookStoreId; //the identity of the sender
+        private Int32 bookStoreId; //the identity of the sender
         private Int32 cardNo; //integer that represents a credit card number
         private Int32 publisherId; //the identity of the receiver
         private Int32 amount; //represents the number of books to order
@@ -17,9 +17,9 @@ namespace P2_598_Doyal_Aletto
         private long milliseconds;  // timestamp  in milliseconds
 
         //Constructor to be used by the encoder
-        public OrderObject(string sender, Int32 cNum, Int32 pubid, Int32 numBooks, double price)
+        public OrderObject(Int32 bsNum, Int32 cNum, Int32 pubid, Int32 numBooks, double price)
         {
-            bookStoreId = sender;
+            bookStoreId = bsNum;
             cardNo = cNum;
             publisherId = pubid;
             amount = numBooks;
@@ -29,9 +29,9 @@ namespace P2_598_Doyal_Aletto
         }
 
         //Constructor to be used by the decoder
-        public OrderObject(string sender, Int32 cNum, Int32 recId, Int32 numBooks, double price, DateTime now)
+        public OrderObject(Int32 bsNum, Int32 cNum, Int32 recId, Int32 numBooks, double price, DateTime now)
         {
-            bookStoreId = sender;
+            bookStoreId = bsNum;
             cardNo = cNum;
             publisherId = recId;
             amount = numBooks;
@@ -40,13 +40,13 @@ namespace P2_598_Doyal_Aletto
         }
 
         //Get senderId
-        public string getSenderId()
+        public Int32 getSenderId()
         {
             return bookStoreId;
         }
 
         //Set senderId
-        public void setSenderId(string s)
+        public void setSenderId(Int32 s)
         {
             bookStoreId = s;
         }
